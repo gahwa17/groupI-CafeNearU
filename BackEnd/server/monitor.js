afdbd8dee9814990b76f7f6181a57cf25beadd4e
PTC1116 <ptchen1116jslearning@gmail.com>
2023-08-21 16:08:14 +0800

    feat(monitor): perform health check on EC2 server

--- BackEnd/server/monitor.js
+++ BackEnd/server/monitor.js
@@ -1,15 +1,27 @@
 const https = require('https');
+const { WebhookClient } = require('discord.js');
+require('dotenv').config();
+
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const serverUrl = `https://13.211.10.154/`;
-const checkInterval = 10000; // 10 sec
+const checkInterval = 180000000;
 
 function performHealthCheck() {
   https
-    .get(serverUrl, (res) => {
+    .get(serverUrl, async (res) => {
       const statusCode = res.statusCode;
+      const webhook = new WebhookClient({
+        url: process.env.DISCORD_WEBHOOK_URL,
+      });
       if (statusCode !== 200) {
-        console.error(`Server is not responding. Status Code: ${statusCode}`);
+        try {
+          await webhook.send(
+            `Server is not responding. Status Code: ${statusCode}`,
+          );
+        } catch (sendError) {
+          console.error('Error sending notification:', sendError.message);
+        }
       } else {
         console.log('Server is healthy.');
       }
